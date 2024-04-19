464a5bae303c6ac33c42c77d954085a0a0c3e0c2
Antony Collett <Antony.Collett@oneadvanced.com>
2024-04-19 13:50:50 +0100

    Finishe at a basic dice rolling gmae to see who rolls highest

--- yahtzee.py
+++ yahtzee.py
@@ -32,16 +32,25 @@ class Player():
         self.scores_used = []
         self.rolls_this_turn = 0
         self.score_sheet = [["Upper Section", {"Aces (Ones): ": 0}],["Lower Section", {"3 of a kind: ": 0}]]
+        self.dice = [Dice(1), Dice(2), Dice(3), Dice(4), Dice(5)]    
     
                 
         
-dice1 = Dice(1)
-dice2 = Dice(2)
-dice3 = Dice(3)
-dice4 = Dice(4)
-dice5 = Dice(5)
+
 
 player1 = Player("Antony")
+player2 = Player("James")
 
-print(player1.name)
-print(player1.score_sheet)
+player1.dice[0].roll_dice()
+print(f"{player1.name} just rolled a {player1.dice[0].current_value}")
+
+
+player2.dice[0].roll_dice()
+print(f"{player2.name} just rolled a {player2.dice[0].current_value}")
+
+if player1.dice[0].current_value > player2.dice[0].current_value:
+    print(f"{player1.name} rolled higher than {player2.name}.")
+elif player1.dice[0].current_value == player2.dice[0].current_value:
+    print(f"{player1.name} and {player2.name} rolled the same, what are the chances?")
+else:
+    print(f"{player2.name} rolled higher than {player1.name}.")
