9dda9f35df1c41afffe55d299c10f01fd006b393
Antony Collett <Antony.Collett@oneadvanced.com>
2024-04-19 14:12:51 +0100

    added roll functionality to player to roll all dice at one

--- yahtzee.py
+++ yahtzee.py
@@ -10,7 +10,7 @@ class Dice():
         self.current_value = -1
         self.held = False
         
-    def roll_dice(self):
+    def roll(self):
         if self.held == False:
             self.current_value = random.randint(1, 6)
         else:
@@ -34,6 +34,12 @@ class Player():
         self.score_sheet = [["Upper Section", {"Aces (Ones): ": 0}],["Lower Section", {"3 of a kind: ": 0}]]
         self.dice = [Dice(1), Dice(2), Dice(3), Dice(4), Dice(5)]    
     
+    def roll_dice(self):
+        self.dice[0].roll()
+        self.dice[1].roll()
+        self.dice[2].roll()
+        self.dice[3].roll()
+        self.dice[4].roll()
                 
         
 
@@ -41,16 +47,16 @@ class Player():
 player1 = Player("Antony")
 player2 = Player("James")
 
-player1.dice[0].roll_dice()
-print(f"{player1.name} just rolled a {player1.dice[0].current_value}")
+player1.roll_dice()
+print(f"{player1.name} rolled {player1.dice[0].current_value}, {player1.dice[1].current_value}, {player1.dice[2].current_value}, {player1.dice[3].current_value}, {player1.dice[4].current_value}")
 
 
-player2.dice[0].roll_dice()
-print(f"{player2.name} just rolled a {player2.dice[0].current_value}")
+player2.roll_dice()
+print(f"{player2.name} rolled {player2.dice[0].current_value}, {player2.dice[1].current_value}, {player2.dice[2].current_value}, {player2.dice[3].current_value}, {player2.dice[4].current_value}")
 
-if player1.dice[0].current_value > player2.dice[0].current_value:
-    print(f"{player1.name} rolled higher than {player2.name}.")
-elif player1.dice[0].current_value == player2.dice[0].current_value:
-    print(f"{player1.name} and {player2.name} rolled the same, what are the chances?")
-else:
-    print(f"{player2.name} rolled higher than {player1.name}.")
+#if player1.dice[0].current_value > player2.dice[0].current_value:
+#    print(f"{player1.name} rolled higher than {player2.name}.")
+#elif player1.dice[0].current_value == player2.dice[0].current_value:
+#    print(f"{player1.name} and {player2.name} rolled the same, what are the chances?")
+#else:
+#    print(f"{player2.name} rolled higher than {player1.name}.")
